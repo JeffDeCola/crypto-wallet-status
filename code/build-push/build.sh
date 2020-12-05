f75e2710955be9ae3d488a251e7dea85e4c50c7f
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2020-12-04 20:41:13 -0800

    major revamp

---
+++ code/build-push/build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -e
+# crypto-wallet-status code build.sh
+
+echo " "
+
+if [ "$1" = "-debug" ]
+then
+    echo "************************************************************************"
+    echo "* build.sh -debug (START) **********************************************"
+    echo "************************************************************************"
+    # set -x enables a mode of the shell where all executed commands are printed to the terminal.
+    set -x
+    echo " "
+else
+    echo "************************************************************************"
+    echo "* build.sh (START) *****************************************************"
+    echo "************************************************************************"
+    echo " "
+fi
+
+echo "cd to where go code is"
+echo "cd .."
+cd ..
+
+echo "Build your docker image using Dockerfile"
+echo "NOTE: The binary is built using this step"
+echo "docker build -f build-push/Dockerfile -t jeffdecola/crypto-wallet-status ."
+docker build -f build-push/Dockerfile -t jeffdecola/crypto-wallet-status .
+echo " "
+
+echo "Check Docker Image size"
+echo "docker images jeffdecola/crypto-wallet-status:latest"
+docker images jeffdecola/crypto-wallet-status:latest
+echo " "
+
+echo "Useful commands:"
+echo "     docker run --name crypto-wallet-status -dit jeffdecola/crypto-wallet-status"
+echo "     docker exec -i -t crypto-wallet-status /bin/bash"
+echo "     docker logs crypto-wallet-status"
+echo " "
+
+echo "************************************************************************"
+echo "* build.sh (END) *******************************************************"
+echo "************************************************************************"
+echo " "
